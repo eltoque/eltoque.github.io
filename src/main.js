5154e0e2bd21e4af7a01ff63e4a00541dfc2948a
dogcalas <dogcalas@gmail.com>
2018-10-14 16:16:47 -0400

    Add buttons downloads

--- src/main.js
+++ src/main.js
@@ -33,6 +33,11 @@ var SocialSharing = require('vue-social-sharing');
 Vue.use(SocialSharing);
 
 import Navbar from "./Navbar.vue"
+import {
+    faFileDownload,
+} from '@fortawesome/free-solid-svg-icons';
+import {dom} from '@fortawesome/fontawesome-svg-core'
+import {library} from '@fortawesome/fontawesome-svg-core'
 
 Vue.use(VueWow)
 Vue.use(Autocomplete)
@@ -48,6 +53,9 @@ Vue.use(VueAnalytics, {
         screenview: true
     }
 })
+
+library.add(faFileDownload)
+dom.watch()
 
 new Vue({
     el: '#app',
@@ -73,6 +81,7 @@ new Vue({
                 case "meth": window.open('https://eltoque.com/como-construimos-el-especial-de-la-constitucion','_blank'); break;
                 case "proy": window.open('https://constitucion.eltoque.com/proyecto.html','_blank'); break;
                 case "vig": window.open('https://constitucion.eltoque.com/vigente.html','_blank'); break;
+                case "off": window.open('https://constitucion.eltoque.com/offline.zip','_blank'); break;
             }
 
         },
